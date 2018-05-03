33c6615a0c556d7f6d0937f14d31a3162e725a2d
Jeremie Deray <jeremie.deray@pal-robotics.com>
2018-05-03 16:34:49 +0200

    add ceres_utils

---
+++ include/manif/ceres/ceres_utils.h
@@ -0,0 +1,54 @@
+#ifndef _MANIF_MANIF_CERES_UTILS_H_
+#define _MANIF_MANIF_CERES_UTILS_H_
+
+#include "manif/ceres/local_parametrization.h"
+#include "manif/ceres/objective.h"
+
+#include <ceres/autodiff_local_parameterization.h>
+#include <ceres/autodiff_cost_function.h>
+
+namespace manif
+{
+
+std::string getReason(const int flag)
+{
+  switch(flag)
+  {
+    case 0: return "DID_NOT_RUN";
+    case 1: return "NO_CONVERGENCE";
+    case 5: return "NUMERICAL_FAILURE";
+    default: return "UNKNOWN";
+  }
+}
+
+template <typename _Manifold>
+std::shared_ptr<ceres::LocalParameterization>
+make_local_parametrization_autodiff()
+{
+  return std::make_shared<
+      ceres::AutoDiffLocalParameterization<
+        LocalParameterization<_Manifold>, _Manifold::RepSize, _Manifold::DoF>>();
+}
+
+template <typename _Manifold, typename... Args>
+std::shared_ptr<ceres::CostFunction>
+make_objective(Args&&... args)
+{
+  return std::make_shared<Objective<_Manifold>>(std::forward<Args>(args)...);
+}
+
+template <typename _Manifold, typename... Args>
+std::shared_ptr<ceres::CostFunction>
+make_objective_autodiff(Args&&... args)
+{
+  constexpr int DoF = _Manifold::DoF;
+  constexpr int RepSize = _Manifold::RepSize;
+
+  return std::make_shared<ceres::AutoDiffCostFunction<Objective<_Manifold>, DoF, RepSize>>(
+      new Objective<_Manifold>(_Manifold(std::forward<Args>(args)...))
+        );
+}
+
+} /* namespace manif */
+
+#endif /* _MANIF_MANIF_CERES_UTILS_H_ */
