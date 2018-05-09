5a2241e449dd9335a68b2c2d1dc47ae1bebac0df
Jeremie Deray <jeremie.deray@pal-robotics.com>
2018-05-09 18:29:10 +0200

    cleaning

--- include/manif/constants.h
+++ include/manif/constants.h
@@ -6,21 +6,9 @@
 namespace manif
 {
 
-//template <typename _Scalar>
-//struct Constants
-//{
-//  static constexpr _Scalar eps      = _Scalar(1e-10);
-//  static constexpr _Scalar eps_sq   = eps*eps;
-//  static constexpr _Scalar eps_sqrt = sqrt(eps);
-
-//  static constexpr _Scalar to_rad =
-//      _Scalar(0.017453292519943295769236907684886127134); // pi / 180
-//  static constexpr _Scalar to_deg =
-//      _Scalar(57.295779513082320876798154814105170332); // 180 / pi
-//};
-
 namespace internal
 {
+
 double constexpr sqrtNewtonRaphson(double x, double curr, double prev)
 {
   return curr == prev
