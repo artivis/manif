4658d9cb5805f8c61ca39d04ef4da1146129e376
artivis <deray.jeremie@gmail.com>
2018-06-27 21:14:32 +0200

    algo includes to manif.h

--- include/manif/manif.h
+++ include/manif/manif.h
@@ -1,10 +1,16 @@
 #ifndef _MANIF_MANIF_H_
 #define _MANIF_MANIF_H_
 
+#include "manif/constants.h"
 #include "manif/functions.h"
+
 #include "manif/SO2.h"
 #include "manif/SE2.h"
 #include "manif/SO3.h"
 #include "manif/SE3.h"
+
+#include "manif/algorithms/average.h"
+#include "manif/algorithms/decasteljau.h"
+#include "manif/algorithms/interpolation.h"
 
 #endif /* _MANIF_MANIF_H_ */
