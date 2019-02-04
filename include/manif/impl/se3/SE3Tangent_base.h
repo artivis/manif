434fddda4f8c86ba0c0b573792b76530701bf1e9
Joan Solà <jsola@iri.upc.edu>
2019-02-04 10:27:03 +0100

    WIP Start fillQ(), not tested

--- include/manif/impl/se3/SE3Tangent_base.h
+++ include/manif/impl/se3/SE3Tangent_base.h
@@ -115,6 +115,11 @@ public:
   {
     return Eigen::Map<SO3Tangent<Scalar>>(coeffs.data()+3);
   }
+
+protected:
+  template<typename _EigenDerived>
+  void fillQ(Eigen::MatrixBase<_EigenDerived>& Q) const;
+
 };
 
 template <typename _Derived>
@@ -269,6 +274,52 @@ SE3TangentBase<_Derived>::ljac() const
 
   return Jl;
 }
+
+template<typename _Derived>
+template<typename _EigenDerived>
+void
+SE3TangentBase<_Derived>::fillQ(Eigen::MatrixBase<_EigenDerived>& Q) const
+{
+    const Scalar theta_sq = asSO3().coeffs().squaredNorm();
+    const Eigen::Matrix<Scalar, 3, 3> V = skew(v());
+    const Eigen::Matrix<Scalar, 3, 3> W = asSO3().hat();
+
+    Scalar A(0.5), B, C, D;
+
+    // Small angle approximation
+    if (theta_sq <= Constants<Scalar>::eps_s)
+    {
+      B =  Scalar(1./6.)  + Scalar(1./120.)  * theta_sq;
+      C = -Scalar(1./24.) + Scalar(1./720.)  * theta_sq;
+      D = -Scalar(1./60.);
+    }
+    else
+    {
+      const Scalar theta     = sqrt(theta_sq);
+      const Scalar sin_theta = sin(theta);
+      const Scalar cos_theta = cos(theta);
+
+      B = (theta - sin_theta) / (theta_sq*theta);
+      C = (Scalar(1) - theta_sq/Scalar(2) - cos_theta) / (theta_sq*theta_sq);
+      D = (C - Scalar(3)*(theta-sin_theta-theta_sq*theta/Scalar(6)) / (theta_sq*theta_sq*theta));
+
+      // http://asrl.utias.utoronto.ca/~tdb/bib/barfoot_ser17_identities.pdf
+  //    C = (theta_sq+Scalar(2)*cos_theta-Scalar(2)) / (Scalar(2)*theta_sq*theta_sq);
+  //    D = (Scalar(2)*theta - Scalar(3)*sin_theta + theta*cos_theta) / (Scalar(2)*theta_sq*theta_sq*theta);
+    }
+
+    /// @note Barfoot14tro Eq. 102
+    const Eigen::Matrix<Scalar, 3, 3> VW  = V * W;
+    const Eigen::Matrix<Scalar, 3, 3> WV  = VW.transpose();       // Note on this change wrt. Barfoot: it happens that V*W = (W*V).transpose() !!!
+    const Eigen::Matrix<Scalar, 3, 3> WVW = WV * W;
+    const Eigen::Matrix<Scalar, 3, 3> VWW = VW * W;
+    Q.noalias() =
+        + A * V
+        + B * (WV + VW + WVW)
+        - C * (VWW - VWW.transpose() - Scalar(3) * WVW)           // Note on this change wrt. Barfoot: it happens that V*W*W = -(W*W*V).transpose() !!!
+        - D * WVW * W;                                            // Note on this change wrt. Barfoot: it happens that W*V*W*W = W*W*V*W !!!
+}
+
 
 template <typename _Derived>
 typename SE3TangentBase<_Derived>::Jacobian
