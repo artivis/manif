44bdfebff0fbc56cb189f680212257dc7f20ea58
Prashanth Ramadoss <prashanthramadoss@gmail.com>
2020-10-26 22:18:50 +0100

    Add SE_2(3) Lie group (#154)
    * add SE_2_3 Lie group for double direct spatial isometry
    
    * add SE_2_3 tests, disabling act() method since its undefined for SE_2_3
    
    * fix se_2_3 getters
    
    * fix se_2_3 act - by default implements rigid motion action on 3d points
    
    * add se_2_3 localization example
    
    * se_2_3: remove unused lines and improve Jacobian allocations
    
    * improve se_2_3 localization example
    
    * se_2_3 localization example: fix initialization for CI failure
    
    * Update README.md
    
    * se_2_3 localization example: Fix equations and improve documentation
    
    * se_2_3 localization example: Fix typos and improve doc
    
    * se_2_3_localization example: Fix doc
    
    * se_2_3_localization example: fix motion integration
    
    * se_2_3_localization example: fix time iterators
    
    * se_2_3_localization example: fix estimation input and accelerometer measurement
    
    * se2_3_localization example: update simulated IMU measurement after motion
    
    * se_2_3_localization example: fix Jacobians in state covariance propagation
    
    * se_2_3_localization example: use manif skew() method
    
    * se_2_3_localization example: rename system model Jacobians

---
+++ include/manif/SE_2_3.h
@@ -0,0 +1,16 @@
+#ifndef _MANIF_SE_2_3_H_
+#define _MANIF_SE_2_3_H_
+
+#include "manif/impl/macro.h"
+#include "manif/impl/lie_group_base.h"
+#include "manif/impl/tangent_base.h"
+
+#include "manif/impl/se_2_3/SE_2_3_properties.h"
+#include "manif/impl/se_2_3/SE_2_3_base.h"
+#include "manif/impl/se_2_3/SE_2_3Tangent_base.h"
+#include "manif/impl/se_2_3/SE_2_3.h"
+#include "manif/impl/se_2_3/SE_2_3Tangent.h"
+#include "manif/impl/se_2_3/SE_2_3_map.h"
+#include "manif/impl/se_2_3/SE_2_3Tangent_map.h"
+
+#endif /* _MANIF_SE_2_3_H_ */
