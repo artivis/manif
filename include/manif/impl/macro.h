b3bda66ae8022345ffdb205dc5621410f75817df
artivis <deray.jeremie@gmail.com>
2020-11-08 13:39:56 -0500

    add macro MANIF_MOVE_NOEXCEPT

--- include/manif/impl/macro.h
+++ include/manif/impl/macro.h
@@ -116,6 +116,9 @@ raise(Args&&... args)
 #define MANIF_MAKE_ALIGNED_OPERATOR_NEW_COND_TYPE(X) \
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW_IF((Eigen::internal::traits<typename X::DataType>::Alignment>0))
 
+#define MANIF_MOVE_NOEXCEPT \
+  EIGEN_NOEXCEPT_IF(std::is_nothrow_move_constructible<Scalar>::value)
+
 #define MANIF_DEFAULT_CONSTRUCTOR(X)  \
   X() = default;                      \
   ~X() = default;                     \
