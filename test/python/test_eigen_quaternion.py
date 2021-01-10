64a730dea4cf25126de8af04e21f6caff11ed6fe
artivis <deray.jeremie@gmail.com>
2021-01-10 18:10:22 -0500

    add a couple quaternion pytest

---
+++ test/python/test_eigen_quaternion.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pytest
+
+from manifpy import Quaternion
+
+
+def test_constructor():
+    q = Quaternion(1,2,3,4)
+    assert 2 == q.x()
+    assert 3 == q.y()
+    assert 4 == q.z()
+    assert 1 == q.w()
+
+def test_identity():
+    q = Quaternion.Identity()
+
+    assert 0 == q.x()
+    assert 0 == q.y()
+    assert 0 == q.z()
+    assert 1 == q.w()
+
+    q_other = Quaternion()
+    q_other.setIdentity()
+
+    assert q.isApprox(q_other)
+    assert q.isApprox(q_other * q_other)
+    assert q.isApprox(q_other.normalized())
+
+    q_inv = q.inverse()
+
+    assert q_inv.isApprox(q_other)
+    assert q_inv.isApprox(q.conjugate())
+
+    assert (3,3) == q.toRotationMatrix().shape
+    assert (np.identity(3) == q.toRotationMatrix()).all()
+
+    assert (3,3) == q.matrix().shape
+    assert (np.identity(3) == q.matrix()).all()
+
+    assert 1 == q.norm()
+    assert 1 == q.squaredNorm()
+    assert 1 == q.dot(q) # cos(0)
+    assert 0 == q.angularDistance(q)
