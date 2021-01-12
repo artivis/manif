d8084095ee3e359d0cea01d9b67f98c760445182
artivis <deray.jeremie@gmail.com>
2021-01-11 19:04:30 -0500

    fix quat.setId

--- test/python/test_eigen_quaternion.py
+++ test/python/test_eigen_quaternion.py
@@ -11,6 +11,24 @@ def test_constructor():
     assert 4 == q.z()
     assert 1 == q.w()
 
+    q = Quaternion(np.array([1,2,3,4]))
+    assert 1 == q.x()
+    assert 2 == q.y()
+    assert 3 == q.z()
+    assert 4 == q.w()
+
+    q = Quaternion(np.identity(3))
+    assert 0 == q.x()
+    assert 0 == q.y()
+    assert 0 == q.z()
+    assert 1 == q.w()
+
+def test_vec():
+    vec = Quaternion(1,2,3,4).vec()
+    assert 2 == vec[0]
+    assert 3 == vec[1]
+    assert 4 == vec[2]
+
 def test_identity():
     q = Quaternion.Identity()
 
