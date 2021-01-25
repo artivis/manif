53aa9ec5a4ad797ecb4d9d5e0d3f35eb3782aa71
artivis <deray.jeremie@gmail.com>
2021-01-25 17:46:21 -0500

    wip doc website

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 DOXYFILE = 'Doxyfile-mcss-site'
 
 LINKS_NAVBAR1 = [
-    ('<a href=\"../cpp/index.html\">C++</a>', []),
-    ('<a href=\"../python/index.html\">Python</a>', []),
+    ('<a href=\"./cpp/index.html\">C++</a>', []),
+    ('<a href=\"./python/index.html\">Python</a>', []),
     ('<a href=\"https://github.com/artivis/manif\">GitHub</a>', [])
 ]
 LINKS_NAVBAR2 = []
--- docs/conf_python.py
+++ docs/conf_python.py
@@ -1,16 +1,18 @@
 PROJECT_TITLE = 'manif'
 PROJECT_SUBTITLE = 'Python API'
 
-MAIN_PROJECT_URL = '../html/index.html'
+MAIN_PROJECT_URL = '../index.html'
 
 INPUT_MODULES = ['manifpy']
 OUTPUT = 'site/python'
 
 LINKS_NAVBAR1 = [
     ('Modules', 'modules', []),
-    ('Classes', 'classes', []),
+    ('Classes', 'classes', [])
+]
+LINKS_NAVBAR2 = [
+    ('C++', '../cpp/index.html', []),
     ('Github', 'https://github.com/artivis/manif', [])
 ]
-LINKS_NAVBAR2 = []
 
 PYBIND11_COMPATIBILITY = True
