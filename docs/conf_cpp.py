06575cc93dfce4f382516d94fbe3c93839b3edc0
artivis <deray.jeremie@gmail.com>
2021-01-25 17:47:10 -0500

    wip doc website

---
+++ docs/conf_cpp.py
@@ -0,0 +1,12 @@
+DOXYFILE = 'Doxyfile-mcss'
+
+LINKS_NAVBAR1 = [
+    ('Pages', 'pages', []),
+    ('Namespaces', 'namespaces', []),
+    ('Classes', 'annotated', []),
+    ('Files', 'files', [])
+]
+LINKS_NAVBAR2 = [
+    ('<a href=\"../python/index.html\">Python</a>', []),
+    ('<a href=\"https://github.com/artivis/manif\">GitHub</a>', [])
+]
