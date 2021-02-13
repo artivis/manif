d31ca07f30119b6cbfce9085f3dc0eff13af9b99
artivis <deray.jeremie@gmail.com>
2021-02-13 15:38:43 -0500

    add Python tests

---
+++ test/python/test_so3.py
@@ -0,0 +1,78 @@
+from manifpy import SO3, SO3Tangent
+
+import numpy as np
+
+
+def test_constructor():
+    state = SO3(0, 0, 0, 1)
+    assert 0 == state.x()
+    assert 0 == state.y()
+    assert 0 == state.z()
+    assert 1 == state.w()
+
+    state = SO3(np.array([0, 0, 0, 1]))
+    assert 0 == state.x()
+    assert 0 == state.y()
+    assert 0 == state.z()
+    assert 1 == state.w()
+
+    state = SO3(0, 0, 0)
+    assert 0 == state.x()
+    assert 0 == state.y()
+    assert 0 == state.z()
+    assert 1 == state.w()
+
+    # state = SO3(Quaternion(1, 0, 0, 0))
+    # assert 0 == state.x()
+    # assert 0 == state.y()
+    # assert 0 == state.z()
+    # assert 1 == state.w()
+
+    # state = SO3(AngleAxis(0, UnitX()))
+    # assert 0 == state.x()
+    # assert 0 == state.y()
+    # assert 0 == state.z()
+    # assert 1 == state.w()
+
+    # delta = SO3Tangent(1,2,3)
+    # assert 1 == delta.x()
+    # assert 2 == delta.y()
+    # assert 3 == delta.z()
+
+
+def test_accessors():
+    state = SO3.Identity()
+
+    assert 0 == state.x()
+    assert 0 == state.y()
+    assert 0 == state.z()
+    assert 1 == state.w()
+
+    delta = SO3Tangent.Zero()
+
+    assert 0 == delta.x()
+    assert 0 == delta.y()
+    assert 0 == delta.z()
+
+
+def test_transform():
+    state = SO3.Identity()
+    transform = state.transform()
+
+    assert (4, 4) == transform.shape
+    assert (np.identity(4) == transform).all()
+
+
+def test_rotation():
+    state = SO3.Identity()
+    rotation = state.rotation()
+
+    assert (3, 3) == rotation.shape
+    assert (np.identity(3) == rotation).all()
+
+
+# def test_quaternion():
+#     state = SO3.Identity()
+#     quaternion = state.quat()
+
+#     assert Quaternion.Identity().isApprox(quaternion)
