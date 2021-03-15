57d906adc49df7f5555acf4c87389fff25dc747d
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-03-15 17:17:29 +0100

    Add translation and quat properties in SE3 python bindings

--- python/bindings_se3.cpp
+++ python/bindings_se3.cpp
@@ -45,11 +45,20 @@ void wrap_SE3(py::module &m)
   SE3.def("transform", &SE3d::transform);
   // SE3.def("isometry", &SE3d::isometry);
   SE3.def("rotation", &SE3d::rotation);
-  // SE3.def("quat", &SE3d::quat);
-  SE3.def(
-    "translation",
-    static_cast<SE3d::Translation (SE3d::*)(void) const>(&SE3d::translation)
-  );
+  SE3.def_property(
+      "translation",
+      static_cast<SE3d::Translation (SE3d::*)(void) const>(&SE3d::translation),
+      static_cast<void (SE3d::*)(const SE3d::Translation&)>(&SE3d::translation));
+  SE3.def_property(
+      "quat",
+      [](const manif::SE3d& se3) -> Eigen::Vector4d { return se3.coeffs().segment<4>(3); },
+      [](manif::SE3d& se3, const Eigen::Vector4d& quaternion) {
+          if(abs(quaternion.norm() - Scalar(1)) >= manif::Constants<Scalar>::eps_s) {
+              throw pybind11::value_error("The quaternion is not normalized!");
+          }
+          se3.quat(quaternion);
+      });
+
   SE3.def("x", &SE3d::x);
   SE3.def("y", &SE3d::y);
   SE3.def("z", &SE3d::z);
