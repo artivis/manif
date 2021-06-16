c462fb87a27292e1782f0cd0b97a27fed03f25fa
Diego Ferigo <diego.ferigo@iit.it>
2021-06-16 20:00:51 +0200

    Update setuptools resources

--- setup.py
+++ setup.py
@@ -1,107 +1,21 @@
-import os
-import platform
-import subprocess
 import sys
 
-import xml.etree.ElementTree as ET
-
-import setuptools
-from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
-
-
-"""
-Modified from https://www.benjack.io/2017/06/12/python-cpp-tests.html
-"""
-
-
-class CMakeExtension(Extension):
-
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-class CMakeBuild(build_ext):
-
-    def run(self):
-        try:
-            # out =
-            subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError(
-                'CMake must be installed to build the following extensions: ' +
-                ', '.join(e.name for e in self.extensions))
-
-        # if platform.system() == "Windows":
-        #     cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-        #                                  out.decode()).group(1))
-        #     if cmake_version < '3.1.0':
-        #         raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
-
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
-
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        if platform.system() == 'Windows':
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                cfg.upper(),
-                extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            cmake_args += ['-DBUILD_PYTHON_BINDINGS=ON']
-            build_args += ['--', '-j6']
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', ''),
-            self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                              cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args,
-                              cwd=self.build_temp)
-
-
-def get_package_xml_version():
-    tree = ET.parse('package.xml')
-    return tree.find('version').text
-
-
-with open('README.md', 'r') as f:
-    long_description = f.read()
+from cmake_build_extension import BuildExtension, CMakeExtension
+from setuptools import setup
 
 setup(
-    name='manifpy',
-    version=get_package_xml_version(),
-    author='Jeremie Deray',
-    author_email='deray.jeremie@gmail.com',
-    description='A small library for Lie theory.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/artivis/manif',
-    license='MIT',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: POSIX :: Linux'
+    ext_modules=[
+        CMakeExtension(
+            name="CMakeProject",
+            install_prefix="manifpy",
+            cmake_depends_on=["pybind11"],
+            disable_editable=True,
+            cmake_configure_options=[
+                "-DCALL_FROM_SETUP_PY:BOOL=ON",
+                "-DBUILD_PYTHON_BINDINGS:BOOL=ON",
+                f"-DPython3_EXECUTABLE:PATH={sys.executable}",
+            ],
+        )
     ],
-    ext_modules=[CMakeExtension('manifpy')],
-    python_requires='>=3.6',
-    cmdclass=dict(build_ext=CMakeBuild),
-    zip_safe=False,
-    install_requires=['numpy']
+    cmdclass=dict(build_ext=BuildExtension),
 )
