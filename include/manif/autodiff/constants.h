cd13806c4fdc6422063b5fc0945b6c7fb929abf6
artivis <deray.jeremie@gmail.com>
2022-08-02 20:15:22 -0400

    specialize constants for autodiff:Real

--- include/manif/autodiff/constants.h
+++ include/manif/autodiff/constants.h
@@ -1,15 +1,21 @@
 #ifndef _MANIF_MANIF_AUTODIFF_CONSTANTS_H_
 #define _MANIF_MANIF_AUTODIFF_CONSTANTS_H_
 
+namespace {
+  // size_t without includes.
+  using size_type = decltype(alignof(char));
+}
+
 namespace autodiff {
 namespace detail {
   template <typename T, typename G> struct Dual;
+  template <size_type N, typename T> class Real;
 } // namespace detail
 } // namespace autodiff
 
 namespace manif {
 
-/// @brief Specialize Constants traits for the float-based autodiff::dual type
+/// @brief Specialize Constants traits for autodiff::Dual type
 template <typename Scalar, typename G>
 struct Constants<autodiff::detail::Dual<Scalar, G>> {
   static const autodiff::detail::Dual<Scalar, G> eps;
@@ -19,6 +25,17 @@ template <typename Scalar, typename G>
 const autodiff::detail::Dual<Scalar, G>
 Constants<autodiff::detail::Dual<Scalar, G>>::eps =
   autodiff::detail::Dual<Scalar, G>(Constants<Scalar>::eps);
+
+/// @brief Specialize Constants traits for autodiff::Real type
+template <size_type N, typename T>
+struct Constants<autodiff::detail::Real<N, T>> {
+  static const autodiff::detail::Real<N, T> eps;
+};
+
+template <size_type N, typename T>
+const autodiff::detail::Real<N, T>
+Constants<autodiff::detail::Real<N, T>>::eps =
+  autodiff::detail::Real<N, T>(Constants<T>::eps);
 
 } // namespace manif
 
