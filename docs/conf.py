e43904a330cceccdbd047b78351d30b186ac0b95
artivis <jeremie.deray@canonical.com>
2025-10-31 11:37:43 +0100

    ignore Scholar links in check
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -207,6 +207,8 @@ linkcheck_ignore = [
     "https://github.com/canonical/ACME/*",
     # @todo(artivis) Update the Ceres doc to account for Ceres 2.2 changes
     'http://ceres-solver.org/nnls_modeling.html#localparameterization',
+    "https://scholar.google.com/scholar?cites=16456301708818968338",
+    "https://scholar.google.com/scholar?cites=1235228860941456363",
 ]
 
 
